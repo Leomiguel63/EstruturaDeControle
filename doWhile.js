bd6b19e9c198cf2589e3daad567a8ea66b218909
Leomiguel63 <leonardomiguelr10@gmail.com>
2023-03-28 09:54:19 -0300

    first commit

---
+++ doWhile.js
@@ -0,0 +1,12 @@
+function getInteiroAleatorio (min, max) {
+    const valor = Math.random * (max - min) + min
+    return Math.floor (valor)
+}
+
+let opcao = 0
+do {
+    opcao = getInteiroAleatorio (-1, 10)
+    console.log(`Opção escolhida fpo ${opcao}`)
+} while (opcao != -1)
+
+console.log ('Até a próxima')
---
+++ if.js
@@ -0,0 +1,5 @@
+function teste2 (num) {
+    if (num > 7); { // cuidado com o ';', não usar com estruturas de controle
+        console.log(num)
+    }
+}
